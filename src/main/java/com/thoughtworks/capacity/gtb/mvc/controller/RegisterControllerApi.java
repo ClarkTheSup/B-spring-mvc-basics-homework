c7d918f7aceaf2bc0beadae2818565fb59e301fa
ClarkTheSup <lknkoqq123@163.com>
2020-09-10 10:07:21 +0800

    [kangning] feat: add RegisterController and User

---
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/controller/RegisterControllerApi.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.capacity.gtb.mvc.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegisterControllerApi {
+    @PostMapping("/register")
+    public void register() {
+
+    }
+}
---
+++ src/main/java/com/thoughtworks/capacity/gtb/mvc/domain/User.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.capacity.gtb.mvc.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+    private String username;
+    private String password;
+    private String email;
+}
